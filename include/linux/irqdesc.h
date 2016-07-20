50f11a39e5639e3fc2b31f3f1ce381d4f7ae9095
Philippe Gerum <rpm@xenomai.org>
2016-07-20 15:06:15 +0200

    genirq: irq_pipeline: add IRQ pipeline core

--- include/linux/irqdesc.h
+++ include/linux/irqdesc.h
@@ -171,6 +171,9 @@ static inline int handle_domain_irq(struct irq_domain *domain,
 {
 	return __handle_domain_irq(domain, hwirq, true, regs);
 }
+
+int do_domain_irq(unsigned int irq, struct pt_regs *regs);
+
 #endif
 
 /* Test to see if a driver has successfully requested an irq */
@@ -247,6 +250,14 @@ static inline bool irq_is_percpu_devid(unsigned int irq)
 
 	desc = irq_to_desc(irq);
 	return desc->status_use_accessors & IRQ_PER_CPU_DEVID;
+}
+
+static inline int irq_is_oob(unsigned int irq)
+{
+	struct irq_desc *desc;
+
+	desc = irq_to_desc(irq);
+	return desc->status_use_accessors & IRQ_OOB;
 }
 
 static inline void
