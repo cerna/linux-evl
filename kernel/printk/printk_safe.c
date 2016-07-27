c4c46d75d584ddb280b7b4f125b54c004895f994
Philippe Gerum <rpm@xenomai.org>
2016-07-27 21:27:59 +0200

    printk: irq_pipeline: safe printk with IRQ pipelining
    Defer printk output not to wreck the hard interrupt state, cause
    unacceptable latency, or risk a deadlock, if either printk() is called
    from the head stage or IRQs are hard disabled on entry (which covers
    the case of running over the root stage holding a hard lock).
    
    Deferral is obtained by following the NMI path in vprintk_func() when
    any of the above conditions is true. This relies on the ability for
    the head stage to submit work requests to the root stage using the
    irq_work_queue() interface.

--- kernel/printk/printk_safe.c
+++ kernel/printk/printk_safe.c
@@ -22,6 +22,7 @@
 #include <linux/cpumask.h>
 #include <linux/irq_work.h>
 #include <linux/printk.h>
+#include <linux/irqstage.h>
 
 #include "internal.h"
 
@@ -351,6 +352,10 @@ void __printk_safe_exit(void)
 
 __printf(1, 0) int vprintk_func(const char *fmt, va_list args)
 {
+	if (irqs_pipelined() && (on_head_stage() ||
+		 (hard_irqs_disabled() && system_state == SYSTEM_RUNNING)))
+		return vprintk_nmi(fmt, args);
+
 	if (this_cpu_read(printk_context) & PRINTK_NMI_CONTEXT_MASK)
 		return vprintk_nmi(fmt, args);
 
