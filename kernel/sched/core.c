93885cc325b3348e43e4709c66ccb682f8b891c9
Philippe Gerum <rpm@xenomai.org>
2018-07-05 08:41:15 +0200

    sched: dovetail: announce CPU affinity change

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1117,10 +1117,13 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	}
 
 	/* Can the task run on the task's current CPU? If so, we're done */
-	if (cpumask_test_cpu(task_cpu(p), new_mask))
+	if (cpumask_test_cpu(task_cpu(p), new_mask)) {
+		dovetail_change_task_affinity(p, task_cpu(p));
 		goto out;
+	}
 
 	dest_cpu = cpumask_any_and(cpu_valid_mask, new_mask);
+	dovetail_change_task_affinity(p, dest_cpu);
 	if (task_running(rq, p) || p->state == TASK_WAKING) {
 		struct migration_arg arg = { p, dest_cpu };
 		/* Need help from migration thread: drop lock and wait. */
