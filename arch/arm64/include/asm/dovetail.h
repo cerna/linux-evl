8dd858f7e5502269a771762b07fe103360914c35
Philippe Gerum <rpm@xenomai.org>
2018-09-13 19:11:56 +0200

    arm64: dovetail: add core support

---
+++ arch/arm64/include/asm/dovetail.h
@@ -0,0 +1,9 @@
+/*
+ * arch/arm/include/asm64/dovetail.h
+ *
+ * Copyright (C) 2018 Philippe Gerum.
+ */
+#ifndef _ASM_ARM64_DOVETAIL_H
+#define _ASM_ARM64_DOVETAIL_H
+
+#endif /* _ASM_ARM64_DOVETAIL_H */
