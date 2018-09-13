4ef4dc8269176f5ac04ddf6ca7c8aba8bfccc6c1
Philippe Gerum <rpm@xenomai.org>
2018-09-13 11:17:06 +0200

    lockdep: irq_pipeline: fix lock debugging conditional

--- include/asm-generic/irq_pipeline.h
+++ include/asm-generic/irq_pipeline.h
@@ -111,7 +111,7 @@ static inline bool irq_pipeline_debug(void)
 
 static inline bool irq_pipeline_debug_locking(void)
 {
-	return irqs_pipelined() && IS_ENABLED(CONFIG_PROVE_LOCKING);
+	return irqs_pipelined() && IS_ENABLED(CONFIG_LOCKDEP);
 }
 
 #endif /* __ASM_GENERIC_IRQ_PIPELINE_H */
