2ca04fdc656ebdb28b671ed78314f3dad0a6ec65
Philippe Gerum <rpm@xenomai.org>
2019-04-24 16:40:19 +0200

    x86: irq_pipeline: add IRQ pipeline core

--- arch/x86/include/asm/apic.h
+++ arch/x86/include/asm/apic.h
@@ -440,13 +440,19 @@ static inline void apic_set_eoi_write(void (*eoi_write)(u32 reg, u32 v)) {}
 
 extern void apic_ack_irq(struct irq_data *data);
 
-static inline void ack_APIC_irq(void)
+static inline void __ack_APIC_irq(void)
 {
 	/*
 	 * ack_APIC_irq() actually gets compiled as a single instruction
 	 * ... yummie.
 	 */
 	apic_eoi();
+}
+
+static inline void ack_APIC_irq(void)
+{
+	if (!irqs_pipelined())
+		__ack_APIC_irq();
 }
 
 static inline unsigned default_get_apic_id(unsigned long x)
