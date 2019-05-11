4958c45a0fb36ca556c082362e66809436c5a095
Philippe Gerum <rpm@xenomai.org>
2019-05-11 15:41:29 +0200

    sched: dovetail: add switch prepare/finish handlers
    We need to call the architecture code for context switch preparation
    and finalization (i.e. switch tail), notably to handle preemption of
    the in-kernel fpu context by out-of-band tasks.
    
    Split arch_dovetail_context_resume() into
    arch_dovetail_prepare_switch() and arch_dovetail_finish_switch(),
    passing them the stage transition information we received on entry to
    dovetail_context_switch().

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8026,6 +8026,7 @@ bool dovetail_context_switch(struct dovetail_altsched_context *out,
 {
 	struct task_struct *next, *prev, *last;
 	struct mm_struct *prev_mm, *next_mm;
+	bool inband = false;
 
 	if (leave_inband) {
 		struct task_struct *tsk = current;
@@ -8040,6 +8041,8 @@ bool dovetail_context_switch(struct dovetail_altsched_context *out,
 		if (IS_ENABLED(CONFIG_KVM))
 			oob_notify_kvm();
 	}
+
+	arch_dovetail_switch_prepare(leave_inband);
 
 	next = in->task;
 	prev = out->task;
@@ -8073,8 +8076,6 @@ bool dovetail_context_switch(struct dovetail_altsched_context *out,
 	if (check_hard_irqs_disabled())
 		hard_irqs_disabled();
 
-	arch_dovetail_context_resume();
-
 	/*
 	 * If we entered this routine for switching to an out-of-band
 	 * task but don't have _TLF_OOB set for the current context
@@ -8089,10 +8090,12 @@ bool dovetail_context_switch(struct dovetail_altsched_context *out,
 				!(preempt_count() & STAGE_MASK));
 			preempt_count_sub(STAGE_OFFSET);
 		}
-		return true;
+		inband = true;
 	}
 
-	return false;
+	arch_dovetail_switch_finish(leave_inband);
+
+	return inband;
 }
 EXPORT_SYMBOL_GPL(dovetail_context_switch);
 
