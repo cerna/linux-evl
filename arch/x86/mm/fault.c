9fe2d1e856d57d9635b0efbe6fba14eb8bcbd69e
Philippe Gerum <rpm@xenomai.org>
2019-05-17 17:32:22 +0200

    x86: dovetail: general rework of trap handling
    We need to postpone entering a fault_entry/exit section until we have
    no other option than calling inband code directly or indirectly.  This
    excludes notify_die() calls which trigger the atomic notifier, and
    that one knows how to handle out-of-band callers.
    
    By doing so, we guarantee that ftrace/kprobe/kgdb breakpoints are
    immediately passed to their respective inband handlers, without
    notifying the companion core which has no business in handling
    those. These inband handlers have to be able to run from out-of-band
    context too. Likewise, light exception fixups due to
    probe_kernel_{read, write} attempts must benefit from the same
    guarantee for the same requirement.
    
    In addition, make sure to apply conditional IRQ enabling/disabling
    before fault_entry() is invoked, since we want to restore the hardware
    interrupt state the kernel wants at fault_exit().
    
    Finally, if CONFIG_PREEMPT is enabled, skip preempt_schedule_irq() if
    the inband state is disabled upon return from common IDT entry
    handlers (paranoid handlers already skip kernel preemption by design).

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -709,7 +709,7 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 	   unsigned long address, int signal, int si_code)
 {
 	struct task_struct *tsk = current;
-	unsigned long flags;
+	unsigned long flags, entry_flags = 0;
 	int sig;
 
 	if (user_mode(regs)) {
@@ -728,7 +728,7 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 		 * the below recursive fault logic only apply to a faults from
 		 * task context.
 		 */
-		if (in_interrupt())
+		if (running_inband() && in_interrupt())
 			return;
 
 		/*
@@ -738,10 +738,19 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 		 * faulting through the emulate_vsyscall() logic.
 		 */
 		if (current->thread.sig_on_uaccess_err && signal) {
+			/*
+			 * If !user_mode(regs), we did not notify the
+			 * pipeline about this fault, do this now
+			 * before we (re-enter inband code.
+			 */
+			entry_flags = pipelined_fault_entry(X86_TRAP_PF, regs);
+
 			set_signal_archinfo(address, error_code);
 
 			/* XXX: hwpoison faults will set the wrong code. */
 			force_sig_fault(signal, si_code, (void __user *)address);
+
+			pipelined_fault_exit(entry_flags);
 		}
 
 		/*
@@ -799,6 +808,14 @@ no_context(struct pt_regs *regs, unsigned long error_code,
 		return;
 
 	/*
+	 * The odds we can survive to this PF over an out-of-band
+	 * context are pretty high, but if a companion core is
+	 * present, we want to let it know so it might try desperate
+	 * fixups as well.
+	 */
+	entry_flags = pipelined_fault_entry(X86_TRAP_PF, regs);
+
+	/*
 	 * Buggy firmware could access regions which might page fault, try to
 	 * recover from such faults.
 	 */
@@ -825,6 +842,9 @@ oops:
 	printk(KERN_DEFAULT "CR2: %016lx\n", address);
 
 	oops_end(flags, regs, sig);
+
+	if (!user_mode(regs))
+		pipelined_fault_exit(entry_flags);
 }
 
 /*
@@ -1495,6 +1515,8 @@ static noinline void
 __do_page_fault(struct pt_regs *regs, unsigned long hw_error_code,
 		unsigned long address)
 {
+	unsigned long flags;
+
 	prefetchw(&current->mm->mmap_sem);
 
 	if (unlikely(kmmio_fault(regs, address)))
@@ -1503,8 +1525,11 @@ __do_page_fault(struct pt_regs *regs, unsigned long hw_error_code,
 	/* Was the fault on kernel-controlled part of the address space? */
 	if (unlikely(fault_in_kernel_space(address)))
 		do_kern_addr_fault(regs, hw_error_code, address);
-	else
+	else {
+		flags = pipelined_fault_entry(X86_TRAP_PF, regs);
 		do_user_addr_fault(regs, hw_error_code, address);
+		pipelined_fault_exit(flags);
+	}
 }
 NOKPROBE_SYMBOL(__do_page_fault);
 
@@ -1525,16 +1550,11 @@ dotraplinkage void
 do_page_fault(struct pt_regs *regs, unsigned long error_code, unsigned long address)
 {
 	enum ctx_state prev_state;
-	unsigned long flags;
-
-	flags = pipelined_fault_entry(X86_TRAP_PF, regs);
 
 	prev_state = exception_enter();
 	trace_page_fault_entries(regs, error_code, address);
 	__do_page_fault(regs, error_code, address);
 	exception_exit(prev_state);
-
-	pipelined_fault_exit(flags);
 }
 NOKPROBE_SYMBOL(do_page_fault);
 
