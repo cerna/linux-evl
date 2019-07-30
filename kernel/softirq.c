9511ff0d1c7226127a2161075756d050de7bace1
Philippe Gerum <rpm@xenomai.org>
2019-07-30 15:45:09 +0200

    genirq: irq_pipeline: switch out-of-band eagerly on IRQ
    For a safe and fast handling of out-of-band interrupts, we have to
    care about the following issues:
    
    - we have to preserve the original flow handling, so that EOI happens
      after the event has been handled, not earlier. Specifically, we
      should not 'release' the interrupt line before this point.
    
    - we have to release the interrupt line (e.g. sending EOI or
      unmasking) _before_ the companion kernel might schedule the
      preempted context out of the current CPU as a result of handling an
      out-of-band event (a rescheduling might happen over
      dovetail_irq_exit()).
    
    - we should always trigger dovetail_irq_exit() from the out-of-band
      stage, so that a companion kernel does not have to resort to
      run_oob_call() for jumping back there before rescheduling. In other
      words, we should not downgrade to the preempted in-band context
      between calls to the out-of-band event handler and
      dovetail_irq_exit().
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -417,6 +417,18 @@ void irq_exit(void)
 	trace_hardirq_exit(); /* must be last! */
 }
 
+void irq_enter_if_inband(void)
+{
+	if (running_inband())
+		irq_enter();
+}
+
+void irq_exit_if_inband(void)
+{
+	if (running_inband())
+		irq_exit();
+}
+
 /*
  * This function must run with irqs disabled!
  */
